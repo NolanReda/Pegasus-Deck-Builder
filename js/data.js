887d4a808194c9448ed1df099b6b1ec17b8646c0
Nolan Reda <nolan.reda@gmail.com>
2022-06-15 20:22:31 +0000

    saving to local storage

--- js/data.js
+++ js/data.js
@@ -10,3 +10,16 @@ var data = {
   },
   resultId: 1
 };
+
+var previousJsonEntry = localStorage.getItem('cardData');
+
+if (previousJsonEntry !== null) {
+  data = JSON.parse(previousJsonEntry);
+}
+
+function handleUnload(event) {
+  var jsonEntry = JSON.stringify(data);
+  localStorage.setItem('cardData', jsonEntry);
+}
+
+window.addEventListener('beforeunload', handleUnload);
