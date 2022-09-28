93820ffa71045921e1211c46acb162991e3ee644
Nolan Reda <nolan.reda@gmail.com>
2022-09-28 21:15:01 +0000

    no reload on form submit when pressing enter

--- js/main.js
+++ js/main.js
@@ -1,80 +1,79 @@
 var $searchBar = document.querySelector('#search-bar');
 var $searchButton = document.querySelector('#search-button');
 var $resultList = document.querySelector('#search-results');
+var $searchForm = document.querySelector('.search-form');
 
 var $response = {};
 
 function searchResults(event) {
-  var searchTime;
-  clearTimeout(searchTime);
-  searchTime = setTimeout(() => {
-    $resultList.replaceChildren('');
-    data.resultId = 1;
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://db.ygoprodeck.com/api/v7/cardinfo.php?name=' + $searchBar.value);
-    xhr.responseType = 'json';
-    xhr.addEventListener('load', function () {
-      $response = xhr.response;
-      if (xhr.status === 400) {
-        var noResult = document.createElement('div');
-        noResult.setAttribute('class', 'row none');
-        var h1 = document.createElement('h1');
-        h1.setAttribute('class', 'no-result-found');
-        var nothing = document.createTextNode('No Results Found');
-        h1.appendChild(nothing);
-        noResult.appendChild(h1);
-        $resultList.appendChild(noResult);
-      }
-      if (!$searchBar.value) {
-        var noSearch = document.createElement('div');
-        noSearch.setAttribute('class', 'row none');
-        var h1i = document.createElement('h1');
-        h1i.setAttribute('class', 'no-result-found');
-        var nothing1 = document.createTextNode('Please search a valid card');
-        h1i.appendChild(nothing1);
-        noSearch.appendChild(h1i);
-        $resultList.appendChild(noSearch);
-        return;
-      }
-      if (xhr.status === 200) {
-        for (let i = 0; i < this.response.data[0].card_images.length; i++) {
-          var div = document.createElement('div');
-          div.setAttribute('class', 'column-fourth card-wrapper');
-          div.setAttribute('data-result-id', data.resultId);
-          data.resultId++;
-          var img = document.createElement('img');
-          img.setAttribute('class', 'card');
-          img.setAttribute('src', this.response.data[0].card_images[i].image_url);
-          img.setAttribute('alt', this.response.data[0].name);
-          div.appendChild(img);
-          var add = document.createElement('button');
-          add.setAttribute('id', 'add-button');
-          add.setAttribute('class', 'add-button');
-          add.innerHTML = 'Add card to deck';
-          div.appendChild(add);
-          var select = document.createElement('select');
-          select.setAttribute('id', 'deck-select');
-          select.setAttribute('class', 'deck-select');
-          var selectOne = document.createElement('option');
-          var blankOption = document.createTextNode('select');
-          selectOne.appendChild(blankOption);
-          select.appendChild(selectOne);
-          for (var deck in data.decks) {
-            var option = document.createElement('option');
-            option.setAttribute('value', deck);
-            var optionText = document.createTextNode(deck);
-            option.appendChild(optionText);
-            select.appendChild(option);
-          }
-          div.appendChild(select);
-          $resultList.appendChild(div);
+  event.preventDefault();
+  $resultList.replaceChildren('');
+  data.resultId = 1;
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://db.ygoprodeck.com/api/v7/cardinfo.php?name=' + $searchBar.value);
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    $response = xhr.response;
+    if (xhr.status === 400) {
+      var noResult = document.createElement('div');
+      noResult.setAttribute('class', 'row none');
+      var h1 = document.createElement('h1');
+      h1.setAttribute('class', 'no-result-found');
+      var nothing = document.createTextNode('No Results Found');
+      h1.appendChild(nothing);
+      noResult.appendChild(h1);
+      $resultList.appendChild(noResult);
+    }
+    if (!$searchBar.value) {
+      var noSearch = document.createElement('div');
+      noSearch.setAttribute('class', 'row none');
+      var h1i = document.createElement('h1');
+      h1i.setAttribute('class', 'no-result-found');
+      var nothing1 = document.createTextNode('Please search a valid card');
+      h1i.appendChild(nothing1);
+      noSearch.appendChild(h1i);
+      $resultList.appendChild(noSearch);
+      return;
+    }
+    if (xhr.status === 200) {
+      for (let i = 0; i < this.response.data[0].card_images.length; i++) {
+        var div = document.createElement('div');
+        div.setAttribute('class', 'column-fourth card-wrapper');
+        div.setAttribute('data-result-id', data.resultId);
+        data.resultId++;
+        var img = document.createElement('img');
+        img.setAttribute('class', 'card');
+        img.setAttribute('src', this.response.data[0].card_images[i].image_url);
+        img.setAttribute('alt', this.response.data[0].name);
+        div.appendChild(img);
+        var add = document.createElement('button');
+        add.setAttribute('id', 'add-button');
+        add.setAttribute('class', 'add-button');
+        add.innerHTML = 'Add card to deck';
+        div.appendChild(add);
+        var select = document.createElement('select');
+        select.setAttribute('id', 'deck-select');
+        select.setAttribute('class', 'deck-select');
+        var selectOne = document.createElement('option');
+        var blankOption = document.createTextNode('select');
+        selectOne.appendChild(blankOption);
+        select.appendChild(selectOne);
+        for (var deck in data.decks) {
+          var option = document.createElement('option');
+          option.setAttribute('value', deck);
+          var optionText = document.createTextNode(deck);
+          option.appendChild(optionText);
+          select.appendChild(option);
         }
+        div.appendChild(select);
+        $resultList.appendChild(div);
       }
-    });
-    xhr.send();
-  }, 1000);
+    }
+  });
+  xhr.send();
 }
 
+$searchForm.addEventListener('submit', searchResults);
 $searchButton.addEventListener('click', searchResults);
 
 var $dataView = document.querySelectorAll('[data-view]');
@@ -183,7 +182,6 @@ $searchResults.addEventListener('click', addCard);
 
 var $modal = document.querySelector('#modal');
 var $ok = document.querySelector('.ok');
-// var $select = document.querySelectorAll('.deck-select');
 
 function open(event) {
   if (event.target.parentElement.children[2].value === 'select') {
